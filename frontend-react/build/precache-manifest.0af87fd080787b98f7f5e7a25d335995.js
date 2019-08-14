fb5c9666eaa77770e5e2a32f19ac9731fe3aef38
AcresGuz <alberto.crespo86@gmail.com>
2019-08-13 21:37:36 -0300

    Add files via upload

---
+++ frontend-react/build/precache-manifest.0af87fd080787b98f7f5e7a25d335995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3773b449ac107d5d8cc53f640a801ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a73cb4532d12daa2e8",
+    "url": "/static/css/main.9add63e6.chunk.css"
+  },
+  {
+    "revision": "dd71775d9b84f20b3426",
+    "url": "/static/js/2.bd58b048.chunk.js"
+  },
+  {
+    "revision": "89a73cb4532d12daa2e8",
+    "url": "/static/js/main.2125f6db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
