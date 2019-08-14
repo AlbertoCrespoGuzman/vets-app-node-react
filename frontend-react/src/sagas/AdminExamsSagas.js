e75a59b9ace3a139aa834a9514a9f55d3e66f16c
Alberto Crespo <alberto@qualityanalytics.io>
2019-08-13 21:16:46 -0300

    adminExamsSagas file name issue

--- frontend-react/src/sagas/AdminExamsSagas.js
+++ frontend-react/src/sagas/AdminExamsSagas.js
@@ -1,8 +1,8 @@
 import { put } from 'redux-saga/effects'
 import  { loadAdminExamsSuccess } from './../actions/actions'
 
-function *getAdminExams(axios, actions){
-    
+function *getAdminExams(axios){
+
     const dados = yield axios.get('/api/files/')
         console.log(dados.data)
         yield put(loadAdminExamsSuccess(dados.data))
