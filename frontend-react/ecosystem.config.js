c2cbb63cccd6869b7d1074782578c55e4dc3c5dc
Alberto Crespo <alberto@qualityanalytics.io>
2019-08-13 21:56:32 -0300

    deploy issue

--- frontend-react/ecosystem.config.js
+++ frontend-react/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = { 
     apps: [{
       name    : "vets-app-node-react-frontend",
-      script  : "serve -s",
-      args    : "build"
+      script  : "npm",
+      args    : "start"
     }]
   }
