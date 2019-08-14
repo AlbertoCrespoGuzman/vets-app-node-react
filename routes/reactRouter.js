3d59ee35d0e7d08b0a19dac0a3d8ee57ea19fc7d
Alberto Crespo <alberto@qualityanalytics.io>
2019-08-13 22:34:37 -0300

    sdf

--- routes/reactRouter.js
+++ routes/reactRouter.js
@@ -2,16 +2,13 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var router = express.Router()
 router.use(bodyParser.json());
+const path = require('path')
 
-router.route('/')
+router.route('/*')
   .get(function (req, res, next){
+    res.sendFile(path.join(__dirname, 'frontend-react/build', 'index.html'));
+  })
 
-    res.send('index2.html')
-  })
-  router.route('/reset_password')
-  .get(function (req, res, next){
-    res.send('index.html')
-  })
   
   
 
