8bdc49b1a8472f1ef4f7e152e8fd6ef5ebd14954
Alberto Crespo <alberto@qualityanalytics.io>
2019-10-07 13:40:40 -0300

    server checker status

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,28 +3,11 @@ var bodyParser = require('body-parser')
 var router = express.Router()
 router.use(bodyParser.json())
 
-router.route('/')
-  .get(function (req, res, next){
-    res.render('index', {  })
-  })
+router.route('/status')
+.get(function (req, res, next){
+  res.status(200).send('{"status": "ok"}')
+})
 
-
-
-  router.route('/alive')
-  .get(function (req, res, next){
-    res.send("I'm alive")
-  })
-
-
-  router.route('/error')
-  .get(function (req, res, next){
-
-    setTimeout(()=> {
-        console.error('forcing error')
-        hoalquetal
-    }, 5000)
-    res.send('forced error this never has to be seen')
-  })
 
 
   module.exports = router
