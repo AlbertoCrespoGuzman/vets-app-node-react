2b859f04bb3adb4d56b1e06a5406bbca4a7ddc33
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 15:54:32 +0200

    sdf

--- script/status-cronjob.py
+++ script/status-cronjob.py
@@ -1,9 +1,23 @@
 import os
 stream = os.popen('sudo service jenkins stop')
 output = stream.read()
-print(output)
-stream = os.popen('pgrep -f node | wc -l')
-output = stream.read()
 
+path = "/home/ubuntu/vets-app-node-react/"
 
-print(int(output))
+stream = os.popen('pgrep -f serve | wc -l')
+if stream.read() < 1:   
+    os.popen('cd '+path).read()
+    os.popen('cd frontend-react').read()
+    os.popen('serve  -s build -l 3000 &').read()
+    print('react running')
+else:
+    print('react stopped')
+    
+stream = os.popen('pgrep -f app | wc -l')
+if stream.read() < 3:   
+    os.popen('cd '+path).read()
+    os.popen('node app &').read()
+    print('server running')
+else:
+    print('server stopped')
+
