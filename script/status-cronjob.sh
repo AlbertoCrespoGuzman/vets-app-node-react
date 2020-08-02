7e9c442f0a0e5c82c27bccbf333eb17cff8e0551
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 14:09:03 +0200

    fix: restart service if it is down

--- script/status-cronjob.sh
+++ script/status-cronjob.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
 SERVICE="jenkins"
 SERVICE2="serve"
 SERVICE3="app"
 PATH="/home/ubuntu/vets-app-node-react/"
 
 
-if sudo systemctl is-active --quiet "$SERVICE" >/dev/null
+if systemctl is-active --quiet "$SERVICE" >/dev/null
 then
     echo "$SERVICE is running, let's stop it"
     sudo service jenkins stop
-    sudo pgrep -x "$SERVICE" >/dev/null && echo "$SERVICE still running" || echo "$SERVICE is stopped successfully"
+    pgrep -x "$SERVICE" >/dev/null && echo "$SERVICE still running" || echo "$SERVICE is stopped successfully"
 
 fi
 
 
 
-if (sudo pgrep -f "$SERVICE2" | wc -l) < 1
+if (pgrep -f "$SERVICE2" | wc -l) < 1
 then
     echo "$SERVICE2 is running"
     > /var/log/log.log
@@ -24,21 +23,21 @@ else
     echo "$SERVICE2 stopped"
     cd "$PATH"
     cd frontend-react
-    sudo serve  -s build -l 3000 &
+    serve  -s build -l 3000 &
     # uncomment to start nginx if stopped
     # systemctl start nginx
     # mail  
 fi
 
 
-if (sudo pgrep -f "$SERVICE3" | wc -l) < 3
+if (pgrep -f "$SERVICE3" | wc -l) < 3
 then
     echo "$SERVICE3 is running"
     > /var/log/log.log
 else
     echo "$SERVICE3 stopped"
     cd "$PATH"
-    sudo node app &
+    node app &
     # uncomment to start nginx if stopped
     # systemctl start nginx
     # mail  
