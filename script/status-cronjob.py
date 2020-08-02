3c0c7af075c5ac4ba9c42a67dbdd563c93eb78ab
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 15:56:25 +0200

    sdf

--- script/status-cronjob.py
+++ script/status-cronjob.py
@@ -9,15 +9,20 @@ if stream.read() < 1:
     os.popen('cd '+path).read()
     os.popen('cd frontend-react').read()
     os.popen('serve  -s build -l 3000 &').read()
-    print('react running')
+    print('react stopped, starting it')
 else:
-    print('react stopped')
+    print('react running')
     
 stream = os.popen('pgrep -f app | wc -l')
 if stream.read() < 3:   
     os.popen('cd '+path).read()
     os.popen('node app &').read()
-    print('server running')
+    print('server stopped, starting it')
 else:
-    print('server stopped')
+    print('server running')
+
+os.popen('sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 3000').read()
+os.popen('sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 3000').read()
+
+
 
