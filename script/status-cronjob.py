3025c536c969c733dba44ffa6e1aba308096dead
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 16:04:34 +0200

    sdf

--- script/status-cronjob.py
+++ script/status-cronjob.py
@@ -5,7 +5,7 @@ output = stream.read()
 path = "/home/ubuntu/vets-app-node-react/"
 
 stream = os.popen('pgrep -f serve | wc -l')
-if stream.read() < 1:   
+if int(stream.read()) < 1:   
     os.popen('cd '+path).read()
     os.popen('cd frontend-react').read()
     os.popen('serve  -s build -l 3000 &').read()
@@ -14,7 +14,7 @@ else:
     print('react running')
     
 stream = os.popen('pgrep -f app | wc -l')
-if stream.read() < 3:   
+if int(stream.read()) < 3:   
     os.popen('cd '+path).read()
     os.popen('node app &').read()
     print('server stopped, starting it')
