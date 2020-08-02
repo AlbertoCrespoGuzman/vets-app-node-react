14ae4f3c1c6286c0ab5a46c4ee56485ffebf69d2
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 16:06:32 +0200

    sdf

--- script/status-cronjob.py
+++ script/status-cronjob.py
@@ -5,6 +5,8 @@ output = stream.read()
 path = "/home/ubuntu/vets-app-node-react/"
 
 stream = os.popen('pgrep -f serve | wc -l')
+print('int(stream.read())')
+print(int(stream.read()))
 if int(stream.read()) < 1:   
     os.popen('cd '+path).read()
     os.popen('cd frontend-react').read()
