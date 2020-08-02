9f4d04b2711903b252dbcbea75fda5faea44f5a8
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 16:30:15 +0200

    sdf

--- script/react-cronjob.py
+++ script/react-cronjob.py
@@ -6,13 +6,15 @@ serve_processes = int(os.popen('pgrep -f serve | wc -l').read())
 print('serve_processes')
 print(os.popen('pgrep -f serve | wc -l').read())
 if serve_processes < 2:   
-    os.popen('cd '+path + 'frontend-react/ && serve  -s build -l 3000 &').read()
+    os.popen('sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 3000').read()
+    os.popen('sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 3000').read()
     print('react stopped, starting it')
+    os.popen('cd '+path + 'frontend-react/ && serve  -s build -l 3000 &').read()
+    
 else:
     print('react running')
     
-os.popen('sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 3000').read()
-os.popen('sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 3000').read()
+
 
 
 
