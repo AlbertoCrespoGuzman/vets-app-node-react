96aa9c43b856f9657572bbaab9aff5443fa80e46
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 12:08:17 +0200

    fix: restart service if it is down

--- script/status-cronjob.sh
+++ script/status-cronjob.sh
@@ -10,3 +10,18 @@ else
     # systemctl start nginx
     # mail  
 fi
+
+
+SERVICE2="jenkins"
+if pgrep -x "$SERVICE2" >/dev/null
+then
+    echo "$SERVICE2 is running, let's stop it"
+    service jenkins stop
+    pgrep -x "$SERVICE2" >/dev/null && echo "$SERVICE still running" || echo "$SERVICE is stopped successfully"
+else
+    echo "$SERVICE2 stopped"
+    node ../app
+    # uncomment to start nginx if stopped
+    # systemctl start nginx
+    # mail  
+fi
