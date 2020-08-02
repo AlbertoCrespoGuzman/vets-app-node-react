1479ee177ae2fb22d0ecc59e1ccf61b53e175c32
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 13:22:33 +0200

    fix: restart service if it is down

--- script/status-cronjob.sh
+++ script/status-cronjob.sh
@@ -3,19 +3,30 @@
 SERVICE="jenkins"
 SERVICE2="serve"
 SERVICE3="app"
+PATH="/home/ubuntu/vets-app-node-react/"
+
 
 if systemctl is-active --quiet "$SERVICE" >/dev/null
 then
     echo "$SERVICE is running, let's stop it"
     sudo service jenkins stop
     pgrep -x "$SERVICE" >/dev/null && echo "$SERVICE still running" || echo "$SERVICE is stopped successfully"
-    
-    cd ..
+
+fi
+
+
+
+if pgrep -f "$SERVICE2" >/dev/null
+then
+    echo "$SERVICE2 is running"
+else
+    echo "$SERVICE2 stopped"
+    cd $PATH
     cd frontend-react
     serve  -s build -l 3000 &
-    sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 3000
-    sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 3000
-
+    # uncomment to start nginx if stopped
+    # systemctl start nginx
+    # mail  
 fi
 
 
@@ -24,10 +35,12 @@ then
     echo "$SERVICE3 is running"
 else
     echo "$SERVICE3 stopped"
-    cd ..
+    cd $PATH
     node app &
     # uncomment to start nginx if stopped
     # systemctl start nginx
     # mail  
 fi
 
+sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 3000
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 3000
