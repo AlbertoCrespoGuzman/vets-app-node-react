0f073e7d779a74e1b3b0403b6f622190d4a5072e
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 12:12:35 +0200

    fix: restart service if it is down

--- script/status-cronjob.sh
+++ script/status-cronjob.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-SERVICE="node"
+
+SERVICE="jenkins"
 if pgrep -x "$SERVICE" >/dev/null
 then
-    echo "$SERVICE is running"
+    echo "$SERVICE is running, let's stop it"
+    service jenkins stop
+    pgrep -x "$SERVICE" >/dev/null && echo "$SERVICE still running" || echo "$SERVICE is stopped successfully"
 else
     echo "$SERVICE stopped"
-    node ../app
-    # uncomment to start nginx if stopped
-    # systemctl start nginx
-    # mail  
-fi
-
-
-SERVICE2="jenkins"
-if pgrep -x "$SERVICE2" >/dev/null
-then
-    echo "$SERVICE2 is running, let's stop it"
-    service jenkins stop
-    pgrep -x "$SERVICE2" >/dev/null && echo "$SERVICE still running" || echo "$SERVICE is stopped successfully"
-else
-    echo "$SERVICE2 stopped"
     node ../app
     # uncomment to start nginx if stopped
     # systemctl start nginx
