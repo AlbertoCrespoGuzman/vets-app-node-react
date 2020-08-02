7c6ef3c111f01ae0b3886c15150d6f6ddbc7dd31
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 16:24:34 +0200

    sdf

--- script/status-cronjob.sh
+++ script/status-cronjob.sh
@@ -1,3 +1,3 @@
-python /home/ubuntu/vets-app-node-react/script/status-cronjob.py &
-python /home/ubuntu/vets-app-node-react/script/react-cronjob.py &
-python /home/ubuntu/vets-app-node-react/script/node-cronjob.py &
+python /home/ubuntu/vets-app-node-react/script/status-cronjob.py
+python /home/ubuntu/vets-app-node-react/script/react-cronjob.py
+python /home/ubuntu/vets-app-node-react/script/node-cronjob.py
