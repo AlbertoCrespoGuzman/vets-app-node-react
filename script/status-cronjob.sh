7788c2ab1373a3d93f2bf17a9c9f09db9896a64e
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 12:19:15 +0200

    fix: restart service if it is down

--- script/status-cronjob.sh
+++ script/status-cronjob.sh
@@ -13,3 +13,31 @@ else
     # systemctl start nginx
     # mail  
 fi
+
+
+SERVICE2="npx"
+if pgrep -x "$SERVICE2" >/dev/null
+then
+    echo "$SERVICE2 is running"
+else
+    echo "$SERVICE2 stopped"
+    serve  -s build -l 3000
+    # uncomment to start nginx if stopped
+    # systemctl start nginx
+    # mail  
+fi
+
+SERVICE3="node"
+if pgrep -x "$SERVICE3" >/dev/null
+then
+    echo "$SERVICE3 is running"
+else
+    echo "$SERVICE3 stopped"
+    node ../app
+    # uncomment to start nginx if stopped
+    # systemctl start nginx
+    # mail  
+fi
+
+sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 4444
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 4444
