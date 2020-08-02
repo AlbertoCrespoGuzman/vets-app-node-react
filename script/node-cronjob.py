f79321209c5a130fb794bdbd39111b6308315489
albertocrespo <alberto.crespo86@gmail.com>
2020-08-02 16:32:01 +0200

    sdf

--- script/node-cronjob.py
+++ script/node-cronjob.py
@@ -6,7 +6,7 @@ path = "/home/ubuntu/vets-app-node-react/"
 app_processes = int(os.popen('pgrep -f app | wc -l').read())
 print('app_processes')
 print(app_processes)
-if app_processes < 3:   
+if app_processes < 6:   
     os.popen('cd /home/ubuntu/vets-app-node-react/ && node app &').read()
     print('server stopped, starting it')
 else:
